8b6f1bbcaae6e82a1653a6854745f6c9e5d6d126
jmmclaug201 <98245823+jmmclaug201@users.noreply.github.com>
2022-10-16 19:59:49 -0400

    Updated to Display Browsing Time by Date

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,7 +1,10 @@
+// Constant Value for no website (i.e. when chrome not open)
+const NO_WEBSITE = "no website"
+
 // Notify When a New Window Takes the Foreground, or Chrome Closes
 chrome.windows.onFocusChanged.addListener((windowId) => {
     if (windowId == chrome.windows.WINDOW_ID_NONE) {
-        updateCurrentPage(null);
+        updateCurrentPage(NO_WEBSITE);
     }
     else {
         chrome.tabs.query({active : true, currentWindow : true})
@@ -39,20 +42,65 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
         const website = changes['current_website'].oldValue,
               start = changes['current_time'].oldValue,
               end = changes['current_time'].newValue;
-        let deltaSeconds = (end - start) / 1000;
+        // Don't add NO_WEBSITE to time_spent (they can track non-chrome stuff
+        // on their own time)
+        if (website === NO_WEBSITE) {
+            return;
+        }
+
         // Get the time_spent dictionary from local storage
         chrome.storage.local.get('time_spent', (items) => {
-            time_dict = items['time_spent'];
-            // Updadte the dictionary, or create a new entry if the entry DNE
+            let time_dict = items['time_spent'];
+            // Update the dictionary, or create a new entry if the entry DNE
             if (!time_dict) {
                 time_dict = {};
             }
-            if (time_dict[website]) {
-                time_dict[website] += deltaSeconds;
+
+            let startDate = new Date(start);
+            let endDate = new Date(end);
+
+            // Set TMP to midnight the day after startdate
+            let tmpDate = new Date(startDate.getFullYear(), 
+                                   startDate.getMonth(), 
+                                   startDate.getDate() + 1);
+            
+            // While tmpDate before endDate 
+            // (while startDate and endDate on different days)
+            while (tmpDate.getTime() < endDate.getTime()) {
+                let day_dict = time_dict[startDate.toDateString()];
+                if (!day_dict) {
+                    day_dict = {}
+                }
+                // Update dayDict with website and time between start and tmp
+                let deltaSeconds = (tmpDate.getTime() - startDate.getTime()) / 1000;
+                if (day_dict[website]) {
+                    day_dict[website] += deltaSeconds;
+                }
+                else {
+                    day_dict[website] =  deltaSeconds;
+                } 
+                time_dict[startDate.toDateString()] = dayDict;
+
+                // set startDate to tmpDate
+                startDate.setTime(tmp.getTime());
+                // set tmpDate to 24 hours later
+                tmpDate.setDate(tmpDate.getDate() + 1);
+            }
+
+            // Finally do the same thing as above for the day endDate is on
+            let day_dict = time_dict[startDate.toDateString()];
+            if (!day_dict) {
+                day_dict = {}
+            }
+            let deltaSeconds = (endDate.getTime() - startDate.getTime()) / 1000;
+            if (day_dict[website]) {
+                day_dict[website] += deltaSeconds;
             }
             else {
-                time_dict[website] =  deltaSeconds;
-            }
+                day_dict[website] =  deltaSeconds;
+            } 
+            time_dict[startDate.toDateString()] = day_dict;
+            
             // Set time_spent to updated time_dict
             chrome.storage.local.set({'time_spent' : time_dict})
             .then(() => {console.log(time_dict)});
@@ -60,14 +108,15 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
     }
 });
 
-// Updates Current Page to new page according to urlString
+// Updates current_website in local storage to new page according to urlString,
+// and updates current_time to the time the website was changed
 function updateCurrentPage(urlString) {
     let url, website;
     // Ensure URL is valid
     try {
-        // null urlString implies no current website (Chrome closed)
-        if (urlString == null) {
-            website == null
+        // NO_WEBSITE urlString implies no current website (Chrome closed)
+        if (urlString == NO_WEBSITE) {
+            website = NO_WEBSITE;
         }
         else {
             url = new URL(urlString);
@@ -78,14 +127,22 @@ function updateCurrentPage(urlString) {
         console.warn("Invalid Urlstring: " + urlString);
         return;
     }
-
-    // Update User's current_website to the new website
-    const d = new Date();
-    let time = d.getTime();
-    chrome.storage.local.set({'current_website' : website,
-                              'current_time' : time})
-        .then(function () {
-            // Debug (Keep?)
-            console.log("current_webiste updated to " + website);
-        });
+    // Get current_webiste from storage to make sure host changed
+    chrome.storage.local.get('current_website')
+    .then((items) => {
+        let oldWebsite = items['current_website'];
+        // Only Update current_website if the website changed
+        if (oldWebsite !== website) {
+            // Update User's current_website to the new website
+            const d = new Date();
+            let time = d.getTime();
+            chrome.storage.local.set({'current_website' : website,
+                                      'current_time' : time})
+            .then(() => {
+                // Debug (Keep?)
+                console.log("current_webiste updated to " + website + " at "
+                             + d.toTimeString());
+            });
+        }
+    });
 }
--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,16 +1,88 @@
-let trackButton = document.getElementById("website-button");
 let clearButton = document.getElementById("clear-button");
-
-// Prints Current Tab
-trackButton.addEventListener("click", async function () { 
-    chrome.tabs.query({active: true}, async (tabs) => {
-        let url = tabs[0].url;
-        alert(url);
-    })
-});
+let getButton = document.getElementById("get-button");
 
 // Clears Time in Local storage time_spent dictionary
 clearButton.addEventListener("click", () => {
+    // If going to keep, also reset timer on current website
     // Error Handle if gonna be a real thing
-    chrome.storage.local.remove('time_spent', () => {});
-})
+    chrome.storage.local.set({'time_spent' : {}}, () => {});
+});
+
+// Handles when button wanting time breakdown is clicked
+getButton.addEventListener("click", () => {
+    // Get Time Spent from Storage
+    chrome.storage.local.get('time_spent')
+    .then((items) => {
+        const time_dict = items['time_spent'];
+        const day_dict = time_dict[new Date(Date.now()).toDateString()];
+        // Get Sorted Time Array
+        const mostViewedRes = getMostViewed(day_dict);
+        const mostViewed = mostViewedRes[0],
+              otherTime = mostViewedRes[1];
+        // Format String for Alerting
+        if (mostViewed.length !== 0) {
+            let displayString = "";
+            Object.entries(mostViewed).forEach(([_, [website, time]]) => {
+                displayString += website + " : " + timeString(time) + "\n";
+            });
+            if (otherTime !== 0) {
+                displayString += "Other Sites : " + timeString(otherTime);
+            }
+            alert(displayString);
+        }
+        else {
+            alert("No Time Logged Today Yet!");
+        }
+    });
+});
+
+// Given an object listing websites and times, returns an array of the websites
+// sorted by time (Length of MAX_SITES or includes all if MAX_SITES = -1)
+function getMostViewed(day_dict, MAX_SITES = 10) {
+    if (!day_dict) {
+        return [[], 0];
+    }
+    let mostViewed = [];
+    let otherTime = 0;
+    // For Each website, find where it belongs on the mostViewed array
+    Object.entries(day_dict).forEach(([key, value]) => {
+        let index = 0;
+        while (mostViewed.length > index && mostViewed[index][1] > value) {
+            index++;
+        }
+        mostViewed.splice(index, 0, [key, value]);
+        // If the array is too long, remove from it
+        if (mostViewed.length > MAX_SITES && MAX_SITES !== -1) {
+            otherTime += mostViewed.pop()[1];
+        }
+    });
+    return [mostViewed, otherTime];
+}
+
+// Turns float duration into a human-readable string
+// i.e. 16322.32 => 4h 32m, 432.12 => 7m 12s
+function timeString(duration) {
+    duration = Math.round(duration);
+    const seconds = duration % 60;
+    duration -= seconds;
+    const minutes = (duration % 3600) / 60;
+    duration -= minutes * 60;
+    const hours = duration / 3600;
+    let timeString = ""
+    if (hours !== 0) {
+        timeString += hours + "h";
+    }
+    if (minutes !== 0) {
+        if (timeString !== "") {
+            timeString += " ";
+        }
+        timeString += minutes + "m";
+    }
+    if (hours === 0 && (minutes === 0 || seconds !== 0)) {
+        if (timeString !== "") {
+            timeString += " ";
+        }
+        timeString += seconds + "s";
+    }
+    return timeString;
+}
